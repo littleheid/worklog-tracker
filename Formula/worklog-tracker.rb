9a41f32d6e173206ac38754a42c005bae66f5e29
heidi <heidi@gmail.com>
2026-06-09 15:06:04 +0800

    release: v0.2.2

--- Formula/worklog-tracker.rb
+++ Formula/worklog-tracker.rb
@@ -2,25 +2,25 @@
 class WorklogTracker < Formula
   desc "Personal local task tracker with SQLite storage"
   homepage "https://github.com/littleheid/worklog-tracker"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.1/worklog-tracker_0.2.1_darwin_arm64"
-      sha256 "090431c2e354b5b393b02107a0b9b1f2c77f1c4a99c8e00d4d5d92b08d2b7a7f"
+      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.2/worklog-tracker_0.2.2_darwin_arm64"
+      sha256 "37e717a9f77206af94466c9a41bcc63ca62445bdb0cbf0e168effd7a0a154146"
     else
-      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.1/worklog-tracker_0.2.1_darwin_amd64"
-      sha256 "7443ac21cc00962d7f37d54659b891f9a42a77807e6f979fa8cf8af2afdec292"
+      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.2/worklog-tracker_0.2.2_darwin_amd64"
+      sha256 "35110d714a813b1bf20a9b2b6f1803253ca09451994568b18ecc1d7af6f31fe2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.1/worklog-tracker_0.2.1_linux_arm64"
-      sha256 "a7eb4a22ad671ddef92534b4cb07de841786a0f37bc62c9e6fec7fd477057975"
+      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.2/worklog-tracker_0.2.2_linux_arm64"
+      sha256 "095c16d5650b3ac94512318aaae2b967d17534397512ce5eb885486bfce4f25a"
     else
-      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.1/worklog-tracker_0.2.1_linux_amd64"
-      sha256 "b36f2f9720c589e49d94348afec9fc5ac88b662f293860866e5c339fbea1a467"
+      url "https://github.com/littleheid/worklog-tracker/releases/download/v0.2.2/worklog-tracker_0.2.2_linux_amd64"
+      sha256 "56c0072acc9fc2f5c2184e0f2a98dc9253b6fdc1e83db11448a78ae55b8c3efe"
     end
   end
 
